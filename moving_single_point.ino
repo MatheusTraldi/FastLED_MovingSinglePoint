4f700ad92ab09583ff4085d326398a609e9ef59c
Matheus Traldi <matheus.traldi.santos@gmail.com>
2019-01-29 22:18:08 -0200

    Add files via upload

---
+++ moving_single_point.ino
@@ -0,0 +1,24 @@
+#include <FastLED.h>
+#define NUM_LEDS 144
+#define DATA_PIN 2
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+}
+
+void loop() {
+  // Erasing all LEDs
+  for(int i = 0; i < NUM_LEDS; i++){
+    leds[i] = CRGB::Black;
+  }
+
+  // Moving
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i - 1] = CRGB::Black;
+    leds[i] = CRGB::Red;
+    FastLED.show();
+    delay(30);
+  }
+}
